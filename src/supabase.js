c63d0b1e0eaf09b406d3f0242af4390318899115
Your Name <your@email.com>
2026-03-01 20:34:35 +0100

    Fix supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL  = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_ANON = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const SUPABASE_URL  = "https://prqhuxhwxpfiweyitqhd.supabase.co";
+const SUPABASE_ANON = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBycWh1eGh3eHBmaXdleWl0cWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzMTY2NTgsImV4cCI6MjA4Nzg5MjY1OH0.FWmib0oF1R5nTGOb5w9v2ezC2-v5CoPmR3sDC1ltgbs";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON);
